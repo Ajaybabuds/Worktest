06334bf698652944a479d2ec4b52ccd403da1c3e
Netscore <82878016+Ajaybabuds@users.noreply.github.com>
2021-12-16 11:01:50 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,8 +6,8 @@ from statsmodels.tsa.ar_model import AutoReg
 import warnings
 
 warnings.filterwarnings('ignore')
-st.title("Welcome to NetScore Analytics")
 st.beta_set_page_config(page_title='J & Y')
+st.title("Welcome to NetScore Analytics")
 c_yr = datetime.today().year
 
 
